d71a7e00bb36526b0c771ed477243e852f355814
Ed Zhang <yxiaoping46@gmail.com>
2025-02-05 22:31:45 -0800

    Cleanup Prisma

--- apps/server/src/database/database.module.ts
+++ apps/server/src/database/database.module.ts
@@ -1,32 +1,8 @@
-import { Logger, Module } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import {
-  loggingMiddleware,
-  PrismaModule,
-  PrismaService,
-  providePrismaClientExceptionFilter,
-} from "nestjs-prisma";
-
-import { Config } from "@/server/config/schema";
+import { Module } from '@nestjs/common';
+import { SupabaseModule } from '../supabase/supabase.module';
 
 @Module({
-  imports: [
-    PrismaModule.forRootAsync({
-      isGlobal: true,
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService<Config>) => ({
-        prismaOptions: { datasourceUrl: configService.get("DATABASE_URL") },
-        middlewares: [
-          loggingMiddleware({
-            logLevel: "debug", // only in development
-            logger: new Logger(PrismaService.name),
-            logMessage: (query) =>
-              `[Query] ${query.model}.${query.action} - ${query.executionTime}ms`,
-          }),
-        ],
-      }),
-    }),
-  ],
-  providers: [providePrismaClientExceptionFilter()],
+  imports: [SupabaseModule],
+  exports: [SupabaseModule],
 })
 export class DatabaseModule {}
